60aaf14a583bd4a1c8999435a9cc4f3edc8db0fc
zhangjinshui-nerveee <jinshui-account@proton.me>
2024-12-30 21:47:04 -0500

    plotly is really slowing down my firefox.. need a better solution

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 import pathlib
+wavedata_path = pathlib.Path('data')
+# create pandas instance
+wavedata_no = 4843
+wavedata = pd.read_csv(wavedata_path/"tek{}.csv".format(wavedata_no), 
+                       low_memory=False, # dtype=float, 
+                       on_bad_lines="skip", 
+                       skiprows=20)
+# use plotly to plot all traces out
+print(wavedata.keys())
+# Create a bar plot
+fig = px.bar(wavedata, x='TIME', y='CH1', title='Sample Bar Plot')
 
-
+# Show the plot
+fig.show()
+# show plot in browser
